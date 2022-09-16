2fbc4d7ca2ce7cfe9a68ee5b4e9a04f9760021d1
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-09-16 17:58:58 -0300

    feat: add keys to return bids and comments

--- src/services/bids/listBidsToAnnouncement.service.ts
+++ src/services/bids/listBidsToAnnouncement.service.ts
@@ -1,7 +1,17 @@
 import { prisma } from "../../prisma/client";
 
 const listBidsToAnnouncementService = async (id: string) => {
-  return await prisma.bids.findMany({ where: { announcementId: id } });
+  return await prisma.bids.findMany({
+    where: { announcementId: id },
+    select: {
+      announcementId: true,
+      date: true,
+      id: true,
+      topBid: true,
+      value: true,
+      user: { select: { id: true, name: true } },
+    },
+  });
 };
 
 export default listBidsToAnnouncementService;
--- src/services/comments/listCommentsToAnnounce.service.ts
+++ src/services/comments/listCommentsToAnnounce.service.ts
@@ -1,7 +1,16 @@
 import { prisma } from "../../prisma/client";
 
 const listCommentToAnnounceService = async (id: string) => {
-  return await prisma.comments.findMany({ where: { announcementId: id } });
+  return await prisma.comments.findMany({
+    where: { announcementId: id },
+    select: {
+      announcementId: true,
+      user: { select: { id: true, name: true } },
+      comment: true,
+      id: true,
+      publishedData: true,
+    },
+  });
 };
 
 export default listCommentToAnnounceService;
