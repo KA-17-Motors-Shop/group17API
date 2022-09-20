b93ba1d82240209263ec1e2ec62b0965e67a643f
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-09-20 19:42:56 -0300

    feat: return all bids of user

--- src/services/bids/listBidsToUser.service.ts
+++ src/services/bids/listBidsToUser.service.ts
@@ -1,7 +1,16 @@
 import { prisma } from "../../prisma/client";
 
 const listBidsToUserService = async (userId: string) => {
-  return await prisma.bids.findMany({ where: { userId } });
+  return await prisma.bids.findMany({
+    where: { userId },
+    select: {
+      date: true,
+      id: true,
+      topBid: true,
+      value: true,
+      announcement: { select: { id: true, title: true, limitDate: true } },
+    },
+  });
 };
 
 export default listBidsToUserService;
