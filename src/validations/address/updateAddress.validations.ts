704e420bc926de95b315b0dfce21fd83a0991102
leonardomarchioro <leonardomarchioro11@gmail.com>
2022-09-23 18:12:13 -0300

    fix: validation address columns: city and state

--- src/validations/address/updateAddress.validations.ts
+++ src/validations/address/updateAddress.validations.ts
@@ -7,10 +7,15 @@ const updateAddressSchema = {
         .shape({
           zipCode: string().matches(/^[0-9]{5}-[0-9]{3}$/, "O cep é inválido"),
           state: string().matches(
-            /^([a-zA-Z]+)$/,
+            /[a-zA-Z\u00C0-\u00FF ]+/i,
             "São aceitos somente letras"
           ),
-          city: string().matches(/^([a-zA-Z]+)$/, "São aceitos somente letras"),
+
+          city: string().matches(
+            /[a-zA-Z\u00C0-\u00FF ]+/i,
+            "São aceitos somente letras"
+          ),
+
           street: string(),
           number: number().typeError("São aceitos somente números"),
           complement: string(),
